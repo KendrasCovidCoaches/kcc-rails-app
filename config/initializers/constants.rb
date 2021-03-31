a4ff5dadd15ca30eefba14ac3501a864a6bc8bf2
allenorton <norton.allene@gmail.com>
2021-03-31 17:24:10 -0500

    initial commit

---
+++ config/initializers/constants.rb
@@ -0,0 +1,44 @@
+ALL_AVAILABILITY = [ '1-2 hours a day', '2-4 hours a day', '4+ hours a day', 'Only on Weekends', 'Full Time' ].freeze
+ALL_NUMBER_OF_VOLUNTEERS = [ '1-10', '10-50', '50-100', '100+' ]
+ALL_LEVELS_OF_URGENCY = [ 'Extremely urgent', 'Very urgent', 'Moderately urgent', 'Somewhat urgent', 'Not urgent' ]
+ALL_LEVELS_OF_EXPOSURE = [ 'I', 'II', 'III', 'IV', 'V' ]
+REMOTE_LOCATION = 'Remote'
+CITY_NAME = 'Sacramento'
+STATE_NAME = 'California'
+ALL_LOCATIONS = [ "#{CITY_NAME} (on site)", "Other #{STATE_NAME} locales (driving)", REMOTE_LOCATION ]
+DOMAIN_NAME = 'ResiTown Sacramento'
+AFFILIATE_EMAIL = 'info@sacconect.com'
+PRIMARY_COLOR = 'red'
+HOME_HEADER = "ResiTown #{CITY_NAME} by Community Connections"
+HOME_SUBHEADER = "This is a #{CITY_NAME}-wide partnership platform, where #{CITY_NAME} residents can volunteer (in-person or remotely) and local non-profits and government can post volunteer needs. Let us unite and fight the pandemic together!"
+INSTAGRAM_LINK = 'https://www.instagram.com/'
+LINKEDIN_LINK = 'https://www.linkedin.com/company/community-connections-sacramento'
+FACEBOOK_LINK = 'https://www.facebook.com/Resitown/'
+TWITTER_LINK = 'http://twitter.com/_Resitown'
+NEWSLETTER_LINK = 'https://landing.mailerlite.com/webforms/landing/p8i0f4 '
+LOCAL_AFFILIATES = 'University of Georgia, UNG, Piedmont College, etc.'
+GITHUB_LINK = 'https://github.com/Community-Connections-Sacramento/resi-town'
+ALL_PROJECT_TYPES = [
+    'Contact tracing',
+    'Reduce spread',
+    'Healthcare',
+    'Medical equipment',
+    'Treatment and vaccine R&D',
+    'Tutoring',
+    'Childcare',
+    'Delivery',
+    'Coordination',
+    'Supply chain and logistics',
+    'First responder',
+    'Counseling/companionship',
+    'Food preparation',
+    'Communications',
+    'Legal',
+    'Policy',
+    'Data gathering',
+    'Other'
+].freeze
+
+BLANK_SLATE_TRAINING_STATUS_URL = 'https://app.blankslatetechnologies.com/sreo/v0.1/app/covidvolunteers/user/check-status'
+VOLUNTEERS_REQUIRED_FOR_FUNDING = 50
+MAX_VOLUNTEERS_FOR_HIGHLIGHT_OFFER = 30
---
+++ config/initializers/team.rb
@@ -0,0 +1,28 @@
+
+# frozen_string_literal: true
+
+TEAM = {
+    'ResiTown Sacramento Board Of Directors': [
+        ['Ryan Skaggs', 'Founder, Executive Director', 'Public health advocate and social entrepreneur dedicated to advancing positive change.'],
+        ['Jessica Lin', 'Director of Community Engagement', 'Health equity champion with an extensive background in global health.'],
+        ['Miriam Sanchez', 'Director of Operations', 'MPH student at CSU Northridge with a background in nutrition and holistic health.'],
+        ['Jin Tran', 'Director of Policy and Planning', 'A social worker passionate about civic engagement and social impact- two areas she has scaled operations for at Facebook.'],
+        ['Ellen Chan', 'Director of Communications', 'Lead of digital marketing at the Federal Reserve Bank of San Francisco and podcast producer.'],
+    ],
+    'Student Ambassador Team': [
+        ['Alyssa Rodriguez', 'Student Ambassador', 'Public Health student at Sacramento State University.'],
+        ['Kira Crawley', 'Student Ambassador', 'Public Health student at Sacramento State University.'],
+        ['Sana Sheikh', 'Student Ambassador', 'Occupational Health student at Sacramento State University.'],
+        ['Vy Cao', 'Student Ambassador', 'Occupational Health student at Sacramento State University.'],
+    ],
+    'City Help + ResiTown Sacramento Development Team': [
+        ['Camden Rogers', 'Platform Manager and Lead Developer', 'Lead Web Developer for City Help and founder of Athens Help With Covid'],
+        ['Allene Norton', 'Software Developer', 'Web Developer for ResiTown Sacramento.'],
+    ],
+    'City Help Team': [
+        ['Tiffany Chan', 'Founder', 'Founder of New Haven Help With Covid'],
+        ['Devin Osborne', 'Community Relations Lead', 'Community relations for New Haven Help With Covid'],
+    ],
+}
+
+
