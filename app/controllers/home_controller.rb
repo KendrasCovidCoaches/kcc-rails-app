69cab809ee533a7405a61d729f5a48f66d8094c4
allenorton <norton.allene@gmail.com>
2021-04-01 16:52:52 -0500

    rename project -> appointment

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,15 @@
 class HomeController < ApplicationController
-  before_action :hydrate_project_categories
+  before_action :hydrate_appointment_categories
   before_action :hide_global_announcements
   before_action :set_bg_white
 
   def index
-      @project_count = Rails.cache.fetch('project_count', expires_in: 1.day) do
-        Project.count
+      @appointment_count = Rails.cache.fetch('appointment_count', expires_in: 1.day) do
+        Appointment.count
       end
-    @project_count_total = @project_count
-    # Display the projects in increments of 50
-    @project_count = (@project_count / 50).floor * 50
+    @appointment_count_total = @appointment_count
+    # Display the appointments in increments of 50
+    @appointment_count = (@appointment_count / 50).floor * 50
 
     @volunteer_count = Rails.cache.fetch('volunteer_count', expires_in: 1.day) do
       User.count
@@ -17,6 +17,6 @@ class HomeController < ApplicationController
 
     @home_header = "#{HOME_HEADER}"
     @home_sub_header = "#{HOME_SUBHEADER}"
-    @featured_projects = Project.get_featured_projects
+    @featured_appointments = Appointment.get_featured_appointments
   end
 end
