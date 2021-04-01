3e72552f1fcc64235362141abfc21ac5b7a03161
allenorton <norton.allene@gmail.com>
2021-04-01 16:56:42 -0500

    rename project -> appointment

--- app/frontend/covid/appointment_form.js
+++ app/frontend/covid/appointment_form.js
@@ -1,43 +1,43 @@
-const ProjectForm = {
+const AppointmentForm = {
   initialize() {
     $(document).on('turbolinks:load', () => {
-      if ($('form.new_project,form.edit_project').length > 0) {
-        $('input[name="project[accepting_volunteers]"]').on('click', function(e) {
-          ProjectForm.updateState();
+      if ($('form.new_appointment,form.edit_appointment').length > 0) {
+        $('input[name="appointment[accepting_volunteers]"]').on('click', function (e) {
+          AppointmentForm.updateState();
         });
 
-        $('input[name="project[accepting_volunteers]"]').on('click', function(e) {
-          ProjectForm.updateState();
+        $('input[name="appointment[accepting_volunteers]"]').on('click', function (e) {
+          AppointmentForm.updateState();
         });
 
-        $('#project_organization_status').on('change', function(e) {
-          ProjectForm.updateState();
+        $('#appointment_organization_status').on('change', function (e) {
+          AppointmentForm.updateState();
         });
 
-        ProjectForm.updateState();
+        AppointmentForm.updateState();
       }
     });
   },
 
   updateState() {
-    if ($('input[name="project[accepting_volunteers]"]:checked').val() == 'true') {
+    if ($('input[name="appointment[accepting_volunteers]"]:checked').val() == 'true') {
       $('.is-accepting-volunteers').show();
-      $('#project_looking_for').attr('required', 'required');
+      $('#appointment_looking_for').attr('required', 'required');
     }
     else {
       $('.is-accepting-volunteers').hide();
-      $('#project_looking_for').removeAttr('required');
+      $('#appointment_looking_for').removeAttr('required');
     }
 
-    if ($('#project_organization_status').val() == 'Non-profit') {
+    if ($('#appointment_organization_status').val() == 'Non-profit') {
       $('.is-non-profit').show();
-      $('#project_ein').attr('required', 'required');
+      $('#appointment_ein').attr('required', 'required');
     } else {
       $('.is-non-profit').hide();
-      $('#project_ein').removeAttr('required');
+      $('#appointment_ein').removeAttr('required');
     }
 
-    if ($('#project_organization_status').val() == 'For-profit') {
+    if ($('#appointment_organization_status').val() == 'For-profit') {
       $('.for-profit-warning').show();
     } else {
       $('.for-profit-warning').hide();
@@ -45,4 +45,4 @@ const ProjectForm = {
   }
 }
 
-export default ProjectForm;
+export default AppointmentForm;
