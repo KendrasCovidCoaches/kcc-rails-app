f8716545bd1d8bb1d1ceccb816cb77699ce13fa5
allenorton <norton.allene@gmail.com>
2021-04-01 16:51:12 -0500

    rename project -> appointment

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,10 +1,10 @@
 class AppointmentsController < ApplicationController
   before_action :authenticate_user!, only: [ :new, :create, :edit, :update, :destroy, :toggle_volunteer, :completed_volunteer, :volunteered, :own, :volunteers ]
-  before_action :set_project, only: [ :show, :edit, :update, :destroy, :toggle_volunteer, :completed_volunteer, :volunteers ]
+  before_action :set_appointment, only: [ :show, :edit, :update, :destroy, :toggle_volunteer, :completed_volunteer, :volunteers ]
   before_action :ensure_owner_or_admin, only: [ :edit, :update, :destroy, :volunteers ]
   before_action :set_filters_open, only: :index
-  before_action :set_projects_query, only: :index
-  before_action :hydrate_project_categories, only: :index
+  before_action :set_appointments_query, only: :index
+  before_action :hydrate_appointment_categories, only: :index
   before_action :ensure_no_legacy_filtering, only: :index
   before_action :set_bg_white, only: [:index, :own, :volunteered]
 
@@ -16,44 +16,44 @@ class AppointmentsController < ApplicationController
     @show_global_announcements = false
     @applied_filters = params.dup
 
-    if request.path != projects_path and params[:category_slug].present?
-      @project_category = Settings.project_categories.find { |category| category.slug == params[:category_slug] }
-      @project_location = Settings.project_locations.find { |location| location.slug == params[:category_slug] }
+    if request.path != appointments_path and params[:category_slug].present?
+      @appointment_category = Settings.appointment_categories.find { |category| category.slug == params[:category_slug] }
+      @appointment_location = Settings.appointment_locations.find { |location| location.slug == params[:category_slug] }
       #byebug
-      raise ActionController::RoutingError, 'Not Found' if @project_category.blank? && @project_location.blank?
+      raise ActionController::RoutingError, 'Not Found' if @appointment_category.blank? && @appointment_location.blank?
 
-      if @project_category.present?
-        @applied_filters[:project_types] = @project_category[:project_types]
-        @featured_projects = Rails.cache.read "project_category_#{@project_category[:name].downcase}_featured_projects"
-        @projects = @projects.tagged_with(params[:category_slug], any: true, on: :categories) if params[:category_slug].present?
+      if @appointment_category.present?
+        @applied_filters[:appointment_types] = @appointment_category[:appointment_types]
+        @featured_appointments = Rails.cache.read "appointment_category_#{@appointment_category[:name].downcase}_featured_appointments"
+        @appointments = @appointments.tagged_with(params[:category_slug], any: true, on: :categories) if params[:category_slug].present?
         #byebug
       end
 
-      if @project_location.present?
-        @applied_filters[:project_types] = @project_location[:project_types]
-        @featured_projects = Rails.cache.read "project_category_#{@project_location[:name].downcase}_featured_projects"
-        @projects = @projects.tagged_with(params[:category_slug], any: true, on: :locations) if params[:category_slug].present?
+      if @appointment_location.present?
+        @applied_filters[:appointment_types] = @appointment_location[:appointment_types]
+        @featured_appointments = Rails.cache.read "appointment_category_#{@appointment_location[:name].downcase}_featured_appointments"
+        @appointments = @appointments.tagged_with(params[:category_slug], any: true, on: :locations) if params[:category_slug].present?
         #byebug
       end
     
     else
-      @featured_projects = Project.get_featured_projects
+      @featured_appointments = Appointment.get_featured_appointments
     end
 
     respond_to do |format|
       format.html do
-        @projects_header = I18n.t('projects_looking_for_volunteers')
-        @projects_subheader = I18n.t('new_or_established_projects_helping_with')
-        @page_title = I18n.t('all_projects')
+        @appointments_header = I18n.t('appointments_looking_for_volunteers')
+        @appointments_subheader = I18n.t('new_or_established_appointments_helping_with')
+        @page_title = I18n.t('all_appointments')
 
-        @projects = @projects.page(params[:page]).per(24)
+        @appointments = @appointments.page(params[:page]).per(24)
 
-        @index_from = (@projects.prev_page || 0) * @projects.limit_value + 1
-        @index_to = [@index_from + @projects.limit_value - 1, @projects.total_count].min
-        @total_count = @projects.total_count
+        @index_from = (@appointments.prev_page || 0) * @appointments.limit_value + 1
+        @index_to = [@index_from + @appointments.limit_value - 1, @appointments.total_count].min
+        @total_count = @appointments.total_count
       end
       format.json do
-        render json: @projects
+        render json: @appointments
       end
     end
   end
@@ -61,60 +61,60 @@ class AppointmentsController < ApplicationController
   def volunteered
     params[:page] ||= 1
 
-    @projects = current_user.volunteered_projects.page(params[:page]).per(25)
-    @index_from = (@projects.prev_page || 0) * @projects.limit_value + 1
-    @index_to = [@index_from + @projects.limit_value - 1, @projects.total_count].min
+    @appointments = current_user.volunteered_appointments.page(params[:page]).per(25)
+    @index_from = (@appointments.prev_page || 0) * @appointments.limit_value + 1
+    @index_to = [@index_from + @appointments.limit_value - 1, @appointments.total_count].min
 
-    @projects_header = I18n.t('volunteered_projects')
-    @projects_subheader = I18n.t('these_are_the_projects_where_you_volunteered')
-    @page_title = I18n.t('volunteered_projects')
+    @appointments_header = I18n.t('volunteered_appointments')
+    @appointments_subheader = I18n.t('these_are_the_appointments_where_you_volunteered')
+    @page_title = I18n.t('volunteered_appointments')
     render action: 'index'
   end
 
   def own
     params[:page] ||= 1
 
-    @projects = current_user.projects.page(params[:page]).per(25)
+    @appointments = current_user.appointments.page(params[:page]).per(25)
 
-    @index_from = (@projects.prev_page || 0) * @projects.limit_value + 1
-    @index_to = [@index_from + @projects.limit_value - 1, @projects.total_count].min
+    @index_from = (@appointments.prev_page || 0) * @appointments.limit_value + 1
+    @index_to = [@index_from + @appointments.limit_value - 1, @appointments.total_count].min
 
-    @projects_header = I18n.t('own_projects')
-    @projects_subheader = I18n.t('these_are_the_projects_you_created')
-    @page_title = I18n.t('own_projects')
+    @appointments_header = I18n.t('own_appointments')
+    @appointments_subheader = I18n.t('these_are_the_appointments_you_created')
+    @page_title = I18n.t('own_appointments')
     render action: 'index'
   end
 
   def show
     respond_to do |format|
       format.html
-      format.json { render json: @project }
+      format.json { render json: @appointment }
     end
   end
 
   def volunteers
     respond_to do |format|
-      format.csv { send_data @project.volunteered_users.to_csv, filename: "volunteers-#{Date.today}.csv" }
+      format.csv { send_data @appointment.volunteered_users.to_csv, filename: "volunteers-#{Date.today}.csv" }
     end
   end
 
   def new
-    @project = Project.new
-    track_event 'Project creation started'
+    @appointment = Appointment.new
+    track_event 'Appointment creation started'
   end
 
   def create
-    @project = current_user.projects.new(project_params)
-    @project.volunteer_location = @project.location_list[0]
+    @appointment = current_user.appointments.new(appointment_params)
+    @appointment.volunteer_location = @appointment.location_list[0]
     respond_to do |format|
-      if @project.save
-        track_event 'Project creation complete'
-        ProjectMailer.with(project: @project).new_project.deliver_now
-        format.html { redirect_to @project, notice: I18n.t('project_was_successfully_created') }
-        format.json { render :show, status: :created, location: @project }
+      if @appointment.save
+        track_event 'Appointment creation complete'
+        AppointmentMailer.with(appointment: @appointment).new_appointment.deliver_now
+        format.html { redirect_to @appointment, notice: I18n.t('appointment_was_successfully_created') }
+        format.json { render :show, status: :created, location: @appointment }
       else
         format.html { render :new }
-        format.json { render json: @project.errors, status: :unprocessable_entity }
+        format.json { render json: @appointment.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -123,131 +123,131 @@ class AppointmentsController < ApplicationController
   end
 
   def update
-    updated = @project.update(project_params)
+    updated = @appointment.update(appointment_params)
 
     respond_to do |format|
       if updated
-        format.html { redirect_to @project, notice: I18n.t('project_was_successfully_updated') }
-        format.json { render :show, status: :ok, location: @project }
+        format.html { redirect_to @appointment, notice: I18n.t('appointment_was_successfully_updated') }
+        format.json { render :show, status: :ok, location: @appointment }
       else
         format.html { render :edit }
-        format.json { render json: @project.errors, status: :unprocessable_entity }
+        format.json { render json: @appointment.errors, status: :unprocessable_entity }
       end
     end
   end
 
   def destroy
-    @project.destroy
+    @appointment.destroy
     respond_to do |format|
-      format.html { redirect_to projects_url, notice: I18n.t('project_was_successfully_deleted') }
+      format.html { redirect_to appointments_url, notice: I18n.t('appointment_was_successfully_deleted') }
       format.json { head :no_content }
     end
   end
 
   def toggle_volunteer
-    if @project.volunteered_users.include?(current_user)
+    if @appointment.volunteered_users.include?(current_user)
       #byebug
-      @project.volunteers.where(user: current_user).destroy_all
+      @appointment.volunteers.where(user: current_user).destroy_all
       flash[:notice] = I18n.t('we_ve_removed_you_from_the_list_of_volunteered_peo')
-      ProjectMailer.with(project: @project, user: current_user).cancel_volunteer.deliver_now
+      AppointmentMailer.with(appointment: @appointment, user: current_user).cancel_volunteer.deliver_now
     else
       params[:volunteer_note] ||= ''
 
-      Volunteer.create(user: current_user, project: @project, note: params[:volunteer_note])
+      Volunteer.create(user: current_user, appointment: @appointment, note: params[:volunteer_note])
 
-      ProjectMailer.with(project: @project, user: current_user, note: params[:volunteer_note]).new_volunteer.deliver_now
+      AppointmentMailer.with(appointment: @appointment, user: current_user, note: params[:volunteer_note]).new_volunteer.deliver_now
 
-      flash[:notice] = I18n.t('thanks_for_volunteering_the_project_owners_will_be')
+      flash[:notice] = I18n.t('thanks_for_volunteering_the_appointment_owners_will_be')
       track_event 'User volunteered'
     end
 
-    redirect_to project_path(@project)
+    redirect_to appointment_path(@appointment)
   end
 
   def completed_volunteer
     #byebug
-    if @project.volunteered_users.include?(current_user)
+    if @appointment.volunteered_users.include?(current_user)
       #byebug
-      @project.volunteers.where(user: current_user).destroy_all
+      @appointment.volunteers.where(user: current_user).destroy_all
       flash[:notice] = I18n.t('completed')
-      #ProjectMailer.with(project: @project, user: current_user).cancel_volunteer.deliver_now
+      #AppointmentMailer.with(appointment: @appointment, user: current_user).cancel_volunteer.deliver_now
     end
 
-    redirect_to volunteered_projects_path
+    redirect_to volunteered_appointments_path
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_project
-      @project = Project.find(params[:id])
+    def set_appointment
+      @appointment = Appointment.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
-    def project_params
-      params.fetch(:project, {}).permit(:name, :organization, :organization_mission, :organization_registered, :level_of_urgency, :level_of_exposure, :description, :participants, :looking_for, :contact, :volunteer_location, :links, :start_date, :end_date, :end_date_recurring, :compensation, :background_screening_required, :progress, :docs_and_demo, :number_of_volunteers, :was_helpful, :exit_comments, :visible, :category_slug, :page, :skill_list => [], :completion_list => [], :category_list => [], :location_list => [], :project_type_list => [], :vol_list => [])
+    def appointment_params
+      params.fetch(:appointment, {}).permit(:name, :organization, :organization_mission, :organization_registered, :level_of_urgency, :level_of_exposure, :description, :participants, :looking_for, :contact, :volunteer_location, :links, :start_date, :end_date, :end_date_recurring, :compensation, :background_screening_required, :progress, :docs_and_demo, :number_of_volunteers, :was_helpful, :exit_comments, :visible, :category_slug, :page, :skill_list => [], :completion_list => [], :category_list => [], :location_list => [], :appointment_type_list => [], :vol_list => [])
     end
 
     def ensure_owner_or_admin
-      if !@project.can_edit?(current_user)
+      if !@appointment.can_edit?(current_user)
         flash[:error] = I18n.t('apologies_you_don_t_have_access_to_this')
-        redirect_to projects_path
+        redirect_to appointments_path
       end
     end
 
-    def set_projects_query
+    def set_appointments_query
       @applied_filters = {}
 
-      @projects = Project
-      @projects = @projects.tagged_with(params[:skills], any: true, on: :skills) if params[:skills].present?
-      @projects = @projects.tagged_with(params[:project_types], any: true, on: :project_types) if params[:project_types].present?
-      @projects = @projects.tagged_with(params[:categories], any: true, on: :categories) if params[:categories].present?
-      @projects = @projects.tagged_with(params[:locations], any: true, on: :locations) if params[:locations].present?
-      @projects = @projects.where(accepting_volunteers: params[:accepting_volunteers] == '1') if params[:accepting_volunteers].present?
-      @projects = @projects.where(highlight: true) if params[:highlight].present?
-      @projects = @projects.where(target_country: params[:target_country]) if params[:target_country].present?
-      @projects = @projects.where(status: params[:status]) if params[:status].present?
+      @appointments = Appointment
+      @appointments = @appointments.tagged_with(params[:skills], any: true, on: :skills) if params[:skills].present?
+      @appointments = @appointments.tagged_with(params[:appointment_types], any: true, on: :appointment_types) if params[:appointment_types].present?
+      @appointments = @appointments.tagged_with(params[:categories], any: true, on: :categories) if params[:categories].present?
+      @appointments = @appointments.tagged_with(params[:locations], any: true, on: :locations) if params[:locations].present?
+      @appointments = @appointments.where(accepting_volunteers: params[:accepting_volunteers] == '1') if params[:accepting_volunteers].present?
+      @appointments = @appointments.where(highlight: true) if params[:highlight].present?
+      @appointments = @appointments.where(target_country: params[:target_country]) if params[:target_country].present?
+      @appointments = @appointments.where(status: params[:status]) if params[:status].present?
 
       if params[:query].present?
-        @projects = @projects.search(params[:query]).left_joins(:volunteers).reorder(nil).group(:id)
+        @appointments = @appointments.search(params[:query]).left_joins(:volunteers).reorder(nil).group(:id)
       else
-        @projects = @projects.left_joins(:volunteers).group(:id)
+        @appointments = @appointments.left_joins(:volunteers).group(:id)
       end
 
       if params[:sort_by]
-        @projects = @projects.order(get_order_param)
+        @appointments = @appointments.order(get_order_param)
       else
-        @projects = @projects.order('highlight DESC, COUNT(volunteers.id) DESC, created_at DESC')
+        @appointments = @appointments.order('highlight DESC, COUNT(volunteers.id) DESC, created_at DESC')
       end
 
-      if params[:project_types].present?
-        @applied_filters[:project_types] = params[:project_types]
-        @projects = @projects.tagged_with(params[:project_types]).left_joins(:volunteers).reorder(nil).group(:id)
+      if params[:appointment_types].present?
+        @applied_filters[:appointment_types] = params[:appointment_types]
+        @appointments = @appointments.tagged_with(params[:appointment_types]).left_joins(:volunteers).reorder(nil).group(:id)
       else
-        @projects = @projects.left_joins(:volunteers).group(:id)
+        @appointments = @appointments.left_joins(:volunteers).group(:id)
       end
 
       if params[:categories].present?
         @applied_filters[:categories] = params[:categories]
-        @projects = @projects.tagged_with(params[:categories]).left_joins(:volunteers).reorder(nil).group(:id)
+        @appointments = @appointments.tagged_with(params[:categories]).left_joins(:volunteers).reorder(nil).group(:id)
       else
-        @projects = @projects.left_joins(:volunteers).group(:id)
+        @appointments = @appointments.left_joins(:volunteers).group(:id)
       end
 
       if params[:locations].present?
         @applied_filters[:locations] = params[:locations]
-        @projects = @projects.tagged_with(params[:locations]).left_joins(:volunteers).reorder(nil).group(:id)
+        @appointments = @appointments.tagged_with(params[:locations]).left_joins(:volunteers).reorder(nil).group(:id)
       else
-        @projects = @projects.left_joins(:volunteers).group(:id)
+        @appointments = @appointments.left_joins(:volunteers).group(:id)
       end
 
       if params[:skills].present?
         @applied_filters[:skills] = params[:skills]
-        @projects = @projects.tagged_with(params[:skills]).left_joins(:volunteers).reorder(nil).group(:id)
+        @appointments = @appointments.tagged_with(params[:skills]).left_joins(:volunteers).reorder(nil).group(:id)
       else
-        @projects = @projects.left_joins(:volunteers).group(:id)
+        @appointments = @appointments.left_joins(:volunteers).group(:id)
       end
 
-      @projects = @projects.includes(:project_types, :skills, :categories, :locations, :volunteers)
+      @appointments = @appointments.includes(:appointment_types, :skills, :categories, :locations, :volunteers)
     end
 
     def ensure_no_legacy_filtering
@@ -257,8 +257,8 @@ class AppointmentsController < ApplicationController
         new_params[:skills] = params[:skills].split(',')
       end
 
-      if params[:project_types].present? and params[:project_types].include? ','
-        new_params[:project_types] = params[:project_types].split(',')
+      if params[:appointment_types].present? and params[:appointment_types].include? ','
+        new_params[:appointment_types] = params[:appointment_types].split(',')
       end
 
       if params[:categories].present? and params[:categories].include? ','
@@ -269,7 +269,7 @@ class AppointmentsController < ApplicationController
         new_params[:locations] = params[:locations].split(',')
       end
 
-      return redirect_to projects_path(new_params) if new_params.present?
+      return redirect_to appointments_path(new_params) if new_params.present?
     end
 
     def get_order_param
