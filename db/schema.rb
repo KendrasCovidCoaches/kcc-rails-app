c24860b3f5fab7b5f1f078faf1216e29d591f221
allenorton <norton.allene@gmail.com>
2021-04-05 18:54:24 -0500

    added request refs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_05_234457) do
+ActiveRecord::Schema.define(version: 2021_04_05_235159) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 2021_04_05_234457) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "note", default: "", null: false
+    t.integer "request_id"
   end
 
   create_table "requests", force: :cascade do |t|
