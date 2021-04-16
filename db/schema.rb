c94bb6a1b5b92c97492ad541d56afd4474d6a4ea
allenorton <norton.allene@gmail.com>
2021-04-16 17:03:24 -0500

    patients migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_16_202949) do
+ActiveRecord::Schema.define(version: 2021_04_16_220103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,7 +72,6 @@ ActiveRecord::Schema.define(version: 2021_04_16_202949) do
 
   create_table "patients", force: :cascade do |t|
     t.integer "user_id"
-    t.integer "appointment_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "note", default: "", null: false
